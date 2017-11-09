a7ae3c2c4c4bf5996b4b5ff9393443a285c11c23
Luis Correa d'Almeida <luis.ca@gmail.com>
2017-11-09 08:12:51 +0000

    fixed specs

--- spec/lib/occupier/pg/connection_spec.rb
+++ spec/lib/occupier/pg/connection_spec.rb
@@ -1,22 +1,15 @@
 describe Occupier::Pg::Connection do
 
-  before :all do
-    connection = Occupier::Pg::Connection.new
-    connection.drop_all
-    connection.create("FF_development_default")
-  end
-
-  let!(:database_name) { "FF_development_default" }
-  let!(:connection) { Occupier::Pg::Connection.new }
-
   it "defaults to development environment" do
+    connection = Occupier::Pg::Connection.new
     expect(connection.environment).to eq "development"
   end
 
-  it "creates a database" do
-    database_name = "FF_development_a"
 
-    connection.drop(database_name) if connection.database_exists?(database_name)
+  let!(:database_name) { "FF_test_default" }
+  let!(:connection) { Occupier::Pg::Connection.new "test" }
+
+  it "creates a database" do
     expect(connection.database_exists?(database_name)).to eq false
 
     connection.create(database_name)
@@ -24,9 +17,7 @@ describe Occupier::Pg::Connection do
   end
 
   it "drops database" do
-    database_name = "FF_development_b"
-
-    connection.connection.exec("CREATE DATABASE #{database_name}")
+    connection.create(database_name)
     expect(connection.database_exists?(database_name)).to eq true
 
     connection.drop(database_name)
@@ -34,16 +25,15 @@ describe Occupier::Pg::Connection do
   end
 
   # it "drops all databases" do
-  #   connection.create("FF_development_g")
-  #   connection.create("FF_development_h")
-  #   connection.create("FF_development_i")
+  #   connection.create("FF_test_g")
+  #   connection.create("FF_test_h")
+  #   connection.create("FF_test_i")
   #   expect(connection.database_names.count).to be > 3
   #   connection.drop_all
   #   expect(connection.database_names.count).to eq 0
   # end
 
   it "resets database" do
-    database_name = "FF_development_c"
     connection.create(database_name)
     connection.reset(database_name)
     expect(connection.database_exists?(database_name)).to eq(true)
@@ -63,7 +53,7 @@ describe Occupier::Pg::Connection do
     end
 
     it "connects to a database" do
-      connection.connect(database_name)
+      connection.create(database_name)
 
       query = connection.connection.exec_params("SELECT current_database()")
       current_database_name = query.first()['current_database']
@@ -74,11 +64,14 @@ describe Occupier::Pg::Connection do
   end
 
   it "knows whether a database exists" do
+    connection.create(database_name)
+
     expect(connection.database_exists?("no_db_here")).to eq false
-    expect(connection.database_exists?("postgres")).to eq true
+    expect(connection.database_exists?(database_name)).to eq true
   end
 
   it "gets all FF database names" do
+    connection.create(database_name)
     expect(connection.database_names).to include database_name.downcase
   end
 
