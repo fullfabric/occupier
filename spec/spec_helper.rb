1ade39761ce01b5a7021a16a612902a2f4cfac1f
luis-ca <luis.ca@gmail.com>
2020-08-07 10:58:59 +0100

    fixed specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,22 @@
 require File.expand_path("../../lib/occupier", __FILE__)
 
-require 'faker'
-require 'yaml'
+require "faker"
+require "yaml"
+require "byebug"
 
 RSpec.configure do |config|
 
-  config.before(:all) do
+  config.filter_run_when_matching :focus
 
+  config.before(:all) do
     config      = YAML.load(ERB.new(File.read('config/mongo.yml')).result)[ "test" ]
     @connection = Mongo::Connection.new(config['host'], config['port'])
-
   end
 
   config.before(:each) do
-
     @connection.database_names.select { |name| name =~ /^FF_test_*/ }.map do |database_name|
       @connection.drop_database database_name
     end
-
   end
-
 
 end
