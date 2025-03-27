956d34cf4cb8f8cf96e058f44f3ac3559ba79c0d
Carlos Soares <CarlosSoares@users.noreply.github.com>
2025-03-27 14:49:10 +0000

    DEV-631 - Remove manually set the safe string and remove initial auto connect rails already does that (#34)
    Co-authored-by: Carlos Soares <carlos.soares@fullfabric.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,12 +10,13 @@ RSpec.configure do |config|
   config.before(:all) do
     config  = YAML.load(ERB.new(File.read('config/mongo.yml')).result)["test"]
     @client = Mongo::Client.new(["#{config['host']}:#{config['port']}"])
-    pg_config = YAML.load(ERB.new(File.read('config/database.yml')).result)['test']
+    @pg_config = YAML.load(ERB.new(File.read('config/database.yml')).result)['test']
 
     @pg_client = Occupier::Postgres::Client.new("test", nil)
   end
 
   config.before(:each) do
+    ActiveRecord::Base.establish_connection(@pg_config)
     @client.database_names.select { |name| name =~ /^FF_test_*/ }.map do |database_name|
       @client.use(database_name).database.drop
     end
